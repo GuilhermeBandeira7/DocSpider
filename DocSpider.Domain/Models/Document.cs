243da8b917d4fafade208c6f39c75a4c1a9251d0
Guilherme Bandeira <guilherme.bandeira@senff.com.br>
2025-07-08 12:23:09 -0300

    Solution Creation and Domain Initial Implementation

---
+++ DocSpider.Domain/Models/Document.cs
@@ -0,0 +1,13 @@
+﻿namespace DocSpider.Domain.Models;
+public class Document
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public string? Type { get; set; }
+    public long? Size { get; set; }
+    public string? Path { get; set; }
+    public byte[]? Content { get; set; }
+    public DateTime? UploadDate { get; set; }
+    public string? UploadedBy { get; set; }
+}
+
---
+++ DocSpider.Domain/Models/User.cs
@@ -0,0 +1,8 @@
+﻿namespace DocSpider.Domain.Models;
+public class User
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; }
+    public List<Document>? Documents { get; set; }
+}
+
